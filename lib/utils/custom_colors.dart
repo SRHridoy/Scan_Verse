b28f443553c8997f7e98cf80e4b8ddc883dde0f7
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2025-01-31 12:00:16 +0600

    Ignore lib folder

--- lib/utils/custom_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-Color primaryBGColor = Color(0xff000000);
-Color secondaryBGColor = Color(0xdd000000);
-Color textColor = Color(0xffffffff);
-Color primaryTextColor = Color(0xffdfff00);
-Color secondaryTextColor = Color(0xff373945);
-Color mainIconColor = Color(0xff3f82c4);
